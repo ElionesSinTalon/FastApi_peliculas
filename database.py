045b710edb056c0574f4b83080e2db323ab8eefc
ElionesSinTalon <pedroagresivo2.0@gmail.com>
2026-05-11 02:15:35 -0600

    Migracion a PostgreSQL

--- database.py
+++ database.py
@@ -1,10 +1,26 @@
+import os
+from dotenv import load_dotenv
+
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-SQL_DATABASE_URL = "sqlite:///./sql_app.db"
+load_dotenv()
 
-engine = create_engine(SQL_DATABASE_URL, connect_args={"check_same_thread": False})
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-SessionLocal = sessionmaker(autocommit=False, bind=engine)
+# Compatibilidad Render/PostgreSQL
+DATABASE_URL = DATABASE_URL.replace(
+    "postgres://",
+    "postgresql://"
+)
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
 
 Base = declarative_base()
